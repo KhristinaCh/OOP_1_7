1ad033b63d82c6405b0f47042315928f156b7af0
Khristina Tastaeva <khristinach@gmail.com>
2021-04-09 00:15:10 +0300

    Задание №2. Атрибуты и взаимодействие классов

--- main.py
+++ main.py
@@ -7,11 +7,30 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
+        self.courses_attached = []
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.courses_attached = []
+        self.grades = {}
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
         self.courses_attached = []
 
     def rate_hw(self, student, course, grade):
@@ -23,32 +42,29 @@ class Mentor:
         else:
             return 'Ошибка'
 
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.courses_in_progress += ['Python']
+#
+# cool_mentor = Mentor('Some', 'Buddy')
+# cool_mentor.courses_attached += ['Python']
+#
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
 
-class Lecturer(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
-        self.courses_attached = []
-
-
-class Reviewer(Mentor):
-    def __init__(self, name, surname):
-        super().__init__(name, surname)
-        self.courses_attached = []
-
-
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
-
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+ivan_ivanov = Student('Ivan', 'Ivanov', 'm')
+ivan_ivanov.courses_in_progress.append('Macroeconomics')
 
 alexey_alexeev = Lecturer('Alexey', 'Alexeev')
+alexey_alexeev.courses_attached.append('Macroeconomics')
+
+ivan_ivanov.rate_hw(alexey_alexeev, 'Macroeconomics', 10)
+ivan_ivanov.rate_hw(alexey_alexeev, 'Macroeconomics', 9)
+
 petr_petrov = Reviewer('Petr', 'Petrov')
 
 # print(best_student.grades)
 # print(alexey_alexeev.__dict__)
 # print(petr_petrov.__dict__)
+# print(alexey_alexeev.grades)
+
