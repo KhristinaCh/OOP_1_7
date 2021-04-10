f2b33a5a908fabcc015f2396fa011d7056c6c5cc
Khristina Tastaeva <khristinach@gmail.com>
2021-04-10 22:34:26 +0300

    Задание №4. Полевые испытания

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+list_of_students = {}
+list_of_lecturers = {}
+
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -8,14 +12,28 @@ class Student:
         self.grades = {}
         self.list_of_all_grades = []
 
+    def add_course(self, course):
+        self.courses_in_progress.append(course)
+        if course in list_of_students:
+            list_of_students[course] += [self]
+        else:
+            list_of_students[course] = [self]
+
+    def finish_course(self, course):
+        self.finished_courses.append(course)
+        self.courses_in_progress.remove(course)
+        if course in list_of_students:
+            list_of_students[course].remove(self)
+        else:
+            'Ошибка'
+
     def rate_hw(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            lecturer.list_of_all_grades.append(grade)
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
-                lecturer.list_of_all_grades.append(grade)
             else:
                 lecturer.grades[course] = [grade]
-                lecturer.list_of_all_grades.append(grade)
         else:
             return 'Ошибка'
 
@@ -25,14 +43,23 @@ class Student:
             total += grade
         return total / len(self.list_of_all_grades)
 
+    def avg_rate_per_course(self, course_calc):
+        total = 0
+        if course_calc in self.grades:
+            for grade in self.grades[course_calc]:
+                total += grade
+        return total / len(self.grades[course_calc])
+
     def __str__(self):
         return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.avg_rate()}\nКурсы в процессе обучения: {", ".join(self.courses_in_progress)}\nЗавершенные курсы: {", ".join(self.finished_courses)}\n'
+
 
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+
 
 class Lecturer(Mentor):
     def __init__(self, name, surname):
@@ -47,8 +74,23 @@ class Lecturer(Mentor):
             total += grade
         return total / len(self.list_of_all_grades)
 
+    def avg_rate_per_course(self, course_calc):
+        total = 0
+        if course_calc in self.grades:
+            for grade in self.grades[course_calc]:
+                total += grade
+        return total / len(self.grades[course_calc])
+
+    def add_course(self, course):
+        self.courses_attached.append(course)
+        if course in list_of_lecturers:
+            list_of_lecturers[course] += [self]
+        else:
+            list_of_lecturers[course] = [self]
+
     def __str__(self):
         return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.avg_rate()}\n'
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -57,81 +99,89 @@ class Reviewer(Mentor):
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            student.list_of_all_grades.append(grade)
             if course in student.grades:
                 student.grades[course] += [grade]
-                student.list_of_all_grades.append(grade)
             else:
                 student.grades[course] = [grade]
-                student.list_of_all_grades.append(grade)
         else:
             return 'Ошибка'
+
+    def add_course(self, course):
+        self.courses_attached.append(course)
 
     def __str__(self):
         return f'Имя: {self.name}\nФамилия: {self.surname}\n'
 
-# best_student = Student('Ruoy', 'Eman', 'your_gender')
-# best_student.courses_in_progress += ['Python']
-#
-# cool_mentor = Reviewer('Some', 'Buddy')
-# cool_mentor.courses_attached += ['Python']
-#
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
 
-# ivan_ivanov = Student('Ivan', 'Ivanov', 'm')
-# ivan_ivanov.courses_in_progress.append('Macroeconomics')
-#
-# alexey_alexeev = Lecturer('Alexey', 'Alexeev')
-# alexey_alexeev.courses_attached.append('Macroeconomics')
-#
-# ivan_ivanov.rate_hw(alexey_alexeev, 'Macroeconomics', 10)
-# ivan_ivanov.rate_hw(alexey_alexeev, 'Macroeconomics', 9)
-#
-# petr_petrov = Reviewer('Petr', 'Petrov')
+def avg_rate_per_student(course_calc):
+    students_list = list_of_students[course_calc]
+    total = 0
+    for student in students_list:
+       total += student.avg_rate_per_course(course_calc)
+    return total / len(students_list)
+
+def avg_rate_per_lecturer(course_calc):
+    lecturers_list = list_of_lecturers[course_calc]
+    total = 0
+    for lecturer in lecturers_list:
+        total += lecturer.avg_rate_per_course(course_calc)
+    return total / len(lecturers_list)
 
 
-some_reviewer = Reviewer('Some', 'Reviewer')
-some_reviewer.courses_attached.append('Python')
+some_reviewer = Reviewer('Adam', 'Adams')
+some_reviewer.add_course('Python')
 
-another_reviewer = Reviewer('Another', 'Reviewer')
-another_reviewer.courses_attached.append('Git')
+another_reviewer = Reviewer('Will', 'Willis')
+another_reviewer.add_course('Git')
 
-some_lecturer = Lecturer('Some', 'Lecturer')
-some_lecturer.courses_attached.append('Python')
+some_lecturer = Lecturer('Ben', 'Bens')
+some_lecturer.add_course('Python')
 
-another_lecturer = Lecturer('Another', 'Lecturer')
-another_lecturer.courses_attached.append('Git')
-another_lecturer.courses_attached.append('Введение в программирование')
+another_lecturer = Lecturer('John', 'Johns')
+another_lecturer.add_course('Git')
+another_lecturer.add_course('Введение в программирование')
 
 some_student = Student('Ruoy', 'Eman', 'm')
-some_student.courses_in_progress += ['Python','Git']
-some_student.finished_courses += ['Введение в программирование']
-another_student = Student('Another', 'Student', 'f')
-another_student.courses_in_progress += ['Python','Введение в программирование']
+some_student.add_course('Python')
+some_student.add_course('Git')
+some_student.add_course('Введение в программирование')
+some_student.finish_course('Введение в программирование')
+
+another_student = Student('John', 'Smith', 'm')
+another_student.add_course('Python')
+another_student.add_course('Введение в программирование')
 
 some_student.rate_hw(some_lecturer, 'Python', 10)
+some_student.rate_hw(some_lecturer, 'Python', 5)
 some_student.rate_hw(another_lecturer, 'Git', 9)
+some_student.rate_hw(another_lecturer, 'Git', 3)
+some_student.rate_hw(another_lecturer, 'Git', 10)
 another_student.rate_hw(another_lecturer, 'Введение в программирование', 5)
+another_student.rate_hw(another_lecturer, 'Введение в программирование', 9)
 some_student.rate_hw(another_lecturer, 'Git', 7)
-# another_student.rate_hw(another_lecturer, 'Git', 3)
 
 some_reviewer.rate_hw(some_student, 'Python', 10)
+some_reviewer.rate_hw(some_student, 'Python', 9)
+some_reviewer.rate_hw(some_student, 'Python', 1)
 another_reviewer.rate_hw(some_student, 'Git', 8)
-# another_reviewer.rate_hw(some_student, 'Python', 6)
+another_reviewer.rate_hw(some_student, 'Git', 6)
 some_reviewer.rate_hw(another_student, 'Python', 4)
-# some_reviewer.rate_hw(some_student, 'Git', 10)
+some_reviewer.rate_hw(another_student, 'Python', 10)
 
-# print(best_student.grades)
-# print(alexey_alexeev.__dict__)
-# print(petr_petrov.__dict__)
-# print(alexey_alexeev.grades)
 # print(some_student.__dict__)
 # print(another_student.__dict__)
 # print(some_lecturer.__dict__)
 # print(another_lecturer.__dict__)
 print(some_reviewer)
+# print(another_reviewer)
 print(some_lecturer)
+# print(another_lecturer)
 print(some_student)
+# print(another_student)
 print(some_lecturer.avg_rate() > another_lecturer.avg_rate())
 print(some_student.avg_rate() > another_student.avg_rate())
+print(avg_rate_per_student('Git'))
+print(avg_rate_per_lecturer('Python'))
+print(some_student.avg_rate_per_course('Python'))
+print(some_lecturer.avg_rate_per_course('Python'))
